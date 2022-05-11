000d9da0169b7ecf7c041ec46da6ac331b8a8dc0
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-11 22:32:42 +0600

    currection clall name

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,6 +4,7 @@ import Info from "./Info";
 import MakeAppointment from "./MakeAppointment";
 import Services from "./Services";
 import Testimonials from "./Testimonials";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Services></Services>
       <MakeAppointment></MakeAppointment>
       <Testimonials></Testimonials>
+      <Footer></Footer>
     </div>
   );
 };
