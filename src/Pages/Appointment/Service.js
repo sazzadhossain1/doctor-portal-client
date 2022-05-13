2f6067e2831ca41e706b0b8f32941d9850807979
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-13 06:43:56 +0600

    firebase done

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -4,8 +4,8 @@ const Service = ({ service, setTreatment }) => {
   const { name, slots } = service;
   return (
     <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title text-secondary">{name}</h2>
+      <div class="card-body text-center">
+        <h2 class="text-xl font-bold text-secondary">{name}</h2>
         <p>
           {slots.length > 0 ? (
             <span>{slots[0]}</span>
@@ -21,7 +21,7 @@ const Service = ({ service, setTreatment }) => {
             for="booking-modal"
             disabled={slots.length === 0}
             onClick={() => setTreatment(service)}
-            className="btn btn-primary text-white uppercase "
+            className="btn btn-sm btn-primary text-white uppercase bg-gradient-to-r from-secondary to-primary"
           >
             Book Appointment
           </label>
